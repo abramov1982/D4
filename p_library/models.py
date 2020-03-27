98da943377750502ac0f18f2ca92f5bc7218bac2
Abramov Ilya <abramovio@mail.ru>
2020-03-27 20:30:26 +0300

    Commit

---
+++ p_library/models.py
@@ -0,0 +1,33 @@
+# Create your models here.
+
+from django.db import models
+
+
+class Author(models.Model):
+    full_name = models.TextField()
+    birth_year = models.SmallIntegerField()
+    country = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    ISBN = models.CharField(max_length=13)
+    title = models.TextField()
+    description = models.TextField()
+    year_release = models.SmallIntegerField()
+    copy_count = models.SmallIntegerField(default='1')
+    price = models.DecimalField(max_digits=7, decimal_places=2, default='0.00')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publisher = models.ForeignKey('Publisher', on_delete=models.CASCADE, null=True, blank=True, related_name='books')
+
+    def __str__(self):
+        return self.title
